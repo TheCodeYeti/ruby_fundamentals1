147031dd64d4635f7153b67dddc9b28ecc000cc7
spencer <spencer.dossett@gmail.com>
2016-05-31 14:17:09 -0400

    used + and converted the tip to string in exercise2.rb

--- exercise2.rb
+++ exercise2.rb
@@ -2,4 +2,4 @@ meal_cost = 55.00
 tip_percent = 15.00
 tip_amount = meal_cost * (tip_percent/100)
 
-puts "A #{tip_percent}% tip on a #{meal_cost} dollar meal is #{tip_amount}."
+puts "A #{tip_percent}% tip on a #{meal_cost} dollar meal is " + tip_amount.to_s + "."
