444f24f459b7feb4d13af900b8bd2d43cba1f72a
spencer <spencer.dossett@gmail.com>
2016-05-31 14:24:50 -0400

    did some math in string interpolation

--- exercise2.rb
+++ exercise2.rb
@@ -3,3 +3,5 @@ tip_percent = 15.00
 tip_amount = meal_cost * (tip_percent/100)
 
 puts "A #{tip_percent}% tip on a #{meal_cost} dollar meal is " + tip_amount.to_s + "."
+
+puts "45628 + 7839 = #{45628 + 7839} bam!"
