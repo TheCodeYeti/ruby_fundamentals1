5cdbbee8ff62bb78ec448bb976471015d8be4068
spencer <spencer.dossett@gmail.com>
2016-05-31 14:44:14 -0400

    added prompt for birth year with some logic to print a response

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,11 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}!"
+
+puts "What year were you born in?"
+year = gets.chomp
+if !!/\A\d+\z/.match(year)
+  puts "#{year.to_i}! wow you're old!"
+else
+  puts "Nice Try!"
+end
