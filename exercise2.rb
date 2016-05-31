992d35f305633c70d5e9101e3c73606c1dfcf014
spencer <spencer.dossett@gmail.com>
2016-05-31 14:28:14 -0400

    puts the value of a complex || statement

--- exercise2.rb
+++ exercise2.rb
@@ -5,3 +5,5 @@ tip_amount = meal_cost * (tip_percent/100)
 puts "A #{tip_percent}% tip on a #{meal_cost} dollar meal is " + tip_amount.to_s + "."
 
 puts "45628 + 7839 = #{45628 + 7839} bam!"
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
